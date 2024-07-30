33b972cdc82c3370b88a791f0a5c7ce8ee8d1ae7
SalmanAliGithub <salmanalijijiga@gmail.com>
2024-07-30 18:55:42 +0300

    written client codes but not working

--- client_esp32/client_esp32.ino
+++ client_esp32/client_esp32.ino
@@ -4,17 +4,24 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  if (!SerialBT.begin("Button_1")) { // Initialize Bluetooth with the name "Button_1"
+  if (!SerialBT.begin("Button_2")) { // Initialize Bluetooth with the name "Button_2"
     Serial.println("An error occurred initializing Bluetooth");
   } else {
     Serial.println("Bluetooth initialized");
   }
-  Serial.println("Button_1 ready for connection");
+  Serial.println("Attempting to connect to Button_1");
+
+  while (!SerialBT.connect("Button_1")) {
+    Serial.println("Failed to connect, retrying...");
+    delay(1000);
+  }
+
+  Serial.println("Connected to Button_1");
 }
 
 void loop() {
   // Send data
-  SerialBT.println("Button_1");
+  SerialBT.println("Button_2");
   delay(1000);
 
   // Read incoming data
